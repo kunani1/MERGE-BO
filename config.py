27957e44058b48952bdf8333017a4b2a812f76f5
kunani1 <131540652+kunani1@users.noreply.github.com>
2024-05-19 12:12:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
+    API_HASH = "2beecc3ee357e6e3f2b2e783d4159f9f"
+    BOT_TOKEN = "1843277705:AAGj9xjvO9jbnC75R8USUKxKjIRmq2k1xlE"
+    TELEGRAM_API = "2929027"
+    OWNER = "959184369"
+    OWNER_USERNAME = "@kids_movies_and_Episodes_founder"
+    PASSWORD = "my"
+    DATABASE_URL = "mongodb+srv://ksai:ksai@cluster0.kayzwoi.mongodb.net/cluster0?retryWrites=true&w=majority"
+    LOGCHANNEL = "-1001856739396" # Add channel id as -100 + Actual ID
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
-    MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
+    MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
